c175a0972dba0a7e6b0e118fd13fe26cff5be142
mahamdeh0 <mahamdehabd@gmail.com>
2024-08-19 17:43:32 +0300

    Added IFlightRepository interface with methods for retrieving and saving flights

---
+++ Airport Ticket Booking/Interfaces/IFlightRepository.cs
@@ -0,0 +1,15 @@
+﻿using Airport_Ticket_Booking.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Airport_Ticket_Booking.Interfaces
+{
+    public interface IFlightRepository
+    {
+        public List<Flight> GetAllFlights();
+        public void SaveFlights(List<Flight> flights);
+    }
+}

0b02027870749bb5ab08ed4077578a9368f4d632
mahamdeh0 <mahamdehabd@gmail.com>
2024-08-19 17:04:07 +0300

    Added properties to the Passenger class

--- Airport Ticket Booking/Models/Passenger.cs
+++ Airport Ticket Booking/Models/Passenger.cs
@@ -8,5 +8,9 @@ namespace Airport_Ticket_Booking.Models
 {
     public class Passenger
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<Booking> Bookings { get; set; } = new List<Booking>();
+
     }
 }

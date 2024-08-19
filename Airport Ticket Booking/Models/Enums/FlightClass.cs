7c35e2770b04a8e0f172715f6966c651504ce956
mahamdeh0 <mahamdehabd@gmail.com>
2024-08-19 17:01:59 +0300

    Added enum to specify the flight class

---
+++ Airport Ticket Booking/Models/Enums/FlightClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Airport_Ticket_Booking.Models.Enums
+{
+
+    public enum FlightClass
+    {
+        Economy,
+        Business,
+        FirstClass
+
+    }
+}

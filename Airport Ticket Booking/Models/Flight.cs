57d3ee99ac1c22016452d99b41ff8e3b33c9e9a6
mahamdeh0 <mahamdehabd@gmail.com>
2024-08-19 17:00:16 +0300

    Added properties to the Flight class

--- Airport Ticket Booking/Models/Flight.cs
+++ Airport Ticket Booking/Models/Flight.cs
@@ -8,5 +8,13 @@ namespace Airport_Ticket_Booking.Models
 {
     public class Flight
     {
+        public int FlightNumber { get; set; }
+        public string DepartureCountry { get; set; }
+        public string DestinationCountry { get; set; }
+        public DateTime DepartureDate { get; set; }
+        public string DepartureAirport { get; set; }
+        public string ArrivalAirport { get; set; }
+        public decimal BasePrice { get; set; }
+
     }
 }

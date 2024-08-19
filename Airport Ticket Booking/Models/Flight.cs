496e873459e26dcb289f5514128385708e5801f9
mahamdeh0 <mahamdehabd@gmail.com>
2024-08-19 16:42:17 +0300

    Initial commit: Set up project structure

---
+++ Airport Ticket Booking/Models/Flight.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Airport_Ticket_Booking.Models
+{
+    public class Flight
+    {
+    }
+}

1cfc7bee903f16cfc2d755fe46b389b1a440c4b8
mahamdeh0 <mahamdehabd@gmail.com>
2024-08-19 17:03:22 +0300

    Added properties to the Booking class

--- Airport Ticket Booking/Models/Booking.cs
+++ Airport Ticket Booking/Models/Booking.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Airport_Ticket_Booking.Models.Enums;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,11 @@ namespace Airport_Ticket_Booking.Models
 {
     public class Booking
     {
+        public int BookingId { get; set; }
+        public int FlightId { get; set; }
+        public int PassengerId { get; set; }
+        public FlightClass Class { get; set; }
+        public decimal Price { get; set; }
     }
 }
+
